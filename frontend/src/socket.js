b53fbe433c3c45ad2a5c3b9e22051693e8a0ee4a
sulyman560 <sulyman560@gmail.com>
2026-03-23 20:53:59 +0600

    your message

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 export const socket = io("https://chat-server-5zt5.onrender.com", {
   autoConnect: false, // 🔥 important
-  path: "/socket.io",
+  //path: "/socket.io",
 });
 
 //https://chat-server-5zt5.onrender.com
