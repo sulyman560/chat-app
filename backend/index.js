9b8a5b3644e3ba14874b9076a4b5c1c46eada68f
sulyman560 <sulyman560@gmail.com>
2026-03-24 13:38:16 +0600

    fix

--- backend/index.js
+++ backend/index.js
@@ -114,5 +114,5 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 server.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
